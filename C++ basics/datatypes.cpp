775b9fd20d1a4829dec0aed9d1e124897085f47d
DevBansal08 <devb6646@gmail.com>
2025-03-07 23:26:16 +0530

    Basics done

---
+++ C++ basics/datatypes.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x = 10;
+    long y = 15;
+    long long z = 1200000000;
+    float x = 0.6;
+    string s1 = "Hey";
+    return 0;
+   
+
+}
---
+++ C++ basics/random.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    // cout << "Dev";
+    int x;
+    cout << "Enter value :";
+    cin >> x;
+    cout << x;
+    return 0;
+
+
+
+}
